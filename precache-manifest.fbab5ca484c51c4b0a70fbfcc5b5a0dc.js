86d1e2aa8add8b9301c9159dbb1eace8e24472b2
aadi30thakur <aadi30.thakur@gmail.com>
2020-10-06 11:33:41 +0530

    Updates

---
+++ precache-manifest.fbab5ca484c51c4b0a70fbfcc5b5a0dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4c97d806574aa5bd9b0b6245ef103bf",
+    "url": "/2048-game/index.html"
+  },
+  {
+    "revision": "7aa8db8ff10a58f3ad38",
+    "url": "/2048-game/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "1d5d2e90eac3c7361c97",
+    "url": "/2048-game/static/js/2.b52b4741.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2048-game/static/js/2.b52b4741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aa8db8ff10a58f3ad38",
+    "url": "/2048-game/static/js/main.7697172e.chunk.js"
+  },
+  {
+    "revision": "34ac6282fe3a50ee5fe9",
+    "url": "/2048-game/static/js/runtime-main.ab1477c0.js"
+  }
+]);
